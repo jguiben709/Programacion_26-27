2438958b9a24bd49356c1d1f592d6854851208b6
jguiben709 <jguiben709@g.educaand.es>
2026-09-21 21:14:41 +0200

    Inicio

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,8 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hola y bienvenidos!");
+
+        System.out.print("Bienvenidos a la Programación IES Jaroso");
 
 
     }
